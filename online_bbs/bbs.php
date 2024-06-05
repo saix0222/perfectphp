24dca22660374b4dcd135447fcf986b40c25cf25
sai <sai@xxx.xxx>
2024-06-05 15:47:56 +0900

    p.193-196 ロジックとビューの分割

--- online_bbs/bbs.php
+++ online_bbs/bbs.php
@@ -60,50 +60,10 @@ try {
     $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
     $stmt = $pdo->prepare($sql);
     $flag = $stmt->execute();
-    $result = $stmt->fetchAll();
+    $posts = $stmt->fetchAll();
 }
 catch (Exception $e) {
         echo 'データベースに接続できません：' . mb_convert_encoding($e->getMessage(), 'UTF-8', 'ASCII,JIS,UTF-8,CP51932,SJIS-win');
 }
 
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html;" charset=utf-8" />
-    <title>ひとこと掲示板</title>
-</head>
-<body>
-    <h1>ひとこと掲示板</h1>
-
-    <form action="bbs.php" method="post">
-        <?php if(count($errors)): ?>
-        <ul class="error_list">
-            <?php foreach($errors as $error): ?>
-            <li>
-                <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-        <?php endif; ?>
-        名前：<input type="text" name="name" /><br />
-        ひとこと：<input type="text" name="comment" size="60" /><br />
-        <input type="submit" name="submit" value="送信" />
-    </form>
-
-    <?php if($result !== false && count($result) > 0): ?>
-    <ul>
-        <?php foreach($result as $post): ?>
-        <li>
-            <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
-            <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
-            - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-    <?php endif; ?>
-    
-</body>
-</html>
+include 'views/bbs_view.php';
